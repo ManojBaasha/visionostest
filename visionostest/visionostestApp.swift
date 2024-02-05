3114692fce8f516f36b18d8745eeffcada08e014
ManojBaasha <manoj.elango123@gmail.com>
2024-02-04 16:42:25 -0800

    Initial Commit

---
+++ visionostest/visionostestApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionostestApp.swift
+//  visionostest
+//
+//  Created by Manoj Elango on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct visionostestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
